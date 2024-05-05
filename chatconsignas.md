58d364f92efe525bb9c6b382f0c1ae563303aa80
andresbuten2002 <butenandres@gmail.com>
2024-05-05 18:41:32 -0300

    chore: Eliminamos archivos repetidos

--- chatconsignas.md
+++
@@ -1,9 +0,0 @@
-# CHAT GENERAL
-
-## Clase 1: Usuarios
-
-## Clase 2: Chat
-
-### método 1: ¿qué pasa cuando se conecta? (Incoming Conection)
-
-### método 2:¿qué pasa cuando se desconecta?
--- mqtt_sub_pub_python/pubs.py
+++
@@ -1,6 +0,0 @@
-import paho.mqtt.publish as publish
-
-broker_address = "localhost"
-topic = "casa/cocina/heladera"
-publish.single(topic, "{ temp:30 }", hostname=broker_address,
-client_id='bocha_pub.py')
